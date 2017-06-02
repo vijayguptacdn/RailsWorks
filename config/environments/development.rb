65ff174dc420189dc8d504d5c2241c5d0a58a969
Vijay Gupta <vijaygupta7789@gmail.com>
2017-06-02 10:07:09 +0530

    still inpro

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,25 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # General Settings
+  config.app_domain = 'somedomain.com'
+
+  # Email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: config.app_domain }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com', 
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: 'someuser',
+    password: 'somepass',
+    authentication: :plain,
+    domain: 'somedomain'
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+
+#https://sourcey.com/rails-4-omniauth-using-devise-with-twitter-facebook-and-linkedin/
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,7 +248,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+   config.omniauth :facebook, "712621548910459", "9f92eb9a63fd09d1d9784968eb1f44e1"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
